9d47d0bc7b4f0d4c8107891336cdfcddf98c2da2
DBogdanovich228 <55573297+DBogdanovich228@users.noreply.github.com>
2019-11-22 00:32:39 +0300

    Add files via upload

---
+++ lab3.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+void main()
+{ int f(int res)
+{   
+	for (int i=0; i<=10; i++)
+	for (int j=0; j<=10-i; j++)
+	if (i+j*3+(10-i-j)*5==res)
+	return 1;
+	return 0;
+}
+int m,r1=1,r3=3,r5=5;
+for (m=11; m<50; m++)
+{   
+if (f(m)) printf("%d = da\n", m);
+else printf ("%d = net\n", m);
+}
+return 0;
+}
